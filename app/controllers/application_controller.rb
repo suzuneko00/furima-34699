bcfef10e2849a1ebe217a3428c5c9286827218e0
suzuneko00 <kanon-white@hotmail.co.jp>
2021-07-13 20:13:56 +0900

    ログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(
-      :sign_up, keys: [:nickname, :email, :birthday, :first_name, :last_name, :first_name_kana, :last_name_kana]
-    )
+   devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :email, :birthday, :first_name, :last_name, :first_name_kana, :last_name_kana])
   end
 end
