bcdd7736e34265167860f4a3673af99ad9ee699b
suzuneko00 <kanon-white@hotmail.co.jp>
2021-07-23 15:34:22 +0900

    last comit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
-      :sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana,
-                       :last_name_kana, :birthday]
+      :sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday]
     )
   end
 end
